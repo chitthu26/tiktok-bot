3aefd5df324d15f36f494f6f1395740ddb402118
chitthu26 <hero71210@gmail.com>
2026-03-16 00:26:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ res.sendStatus(200);
 
 });
 
-app.listen(3000,()=>{
-console.log("Server started");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("TikTok Bot Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started");
 });
