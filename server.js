84002e4c0f42d4c79d4ff7e589c26144f5095b9f
chitthu26 <hero71210@gmail.com>
2026-03-16 00:42:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,21 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-res.send("TikTok Bot Running");
-});
-
-app.post("/webhook",(req,res)=>{
-
-let comment = req.body.comment;
-
-if(comment && comment.includes("price")){
-console.log("Auto reply triggered");
-}
-
-res.sendStatus(200);
-
-});
-
-const express = require("express");
-const app = express();
-
+// Home route
 app.get("/", (req, res) => {
   res.send("TikTok Bot Running");
+});
+
+// Webhook route
+app.post("/webhook", (req, res) => {
+
+  let comment = req.body.comment;
+
+  if (comment && comment.includes("price")) {
+    console.log("Auto reply triggered");
+  }
+
+  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
